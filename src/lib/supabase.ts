fb84cab54de230c8e5ff49085ede427ff9fdf4d8
arrizarisky <mhdarriza1404@gmail.com>
2026-04-10 01:02:14 +0700

    update about .env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -10,6 +10,6 @@ if (!supabaseUrl || !supabaseAnonKey) {
 }
 
 export const supabase = createClient(
-  supabaseUrl || 'https://xxdzddbaqyvcdjopxmjy.supabase.co',
-  supabaseAnonKey || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4ZHpkZGJhcXl2Y2Rqb3B4bWp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MTg2MDUsImV4cCI6MjA5MTI5NDYwNX0.wtdns5FUILFwJvQkhlVEQ6hRiD6dP4QjXHBJ7xyT_0c'
+  supabaseUrl ,
+  supabaseAnonKey
 );
